04cfbc7ecbf18e20c39c37acca1910a915998205
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2024-01-10 14:55:31 -0700

    returns external links
    stash
    
    stash
    
    stash

---
+++ wikipedia_companies.py
@@ -0,0 +1,17 @@
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "query",
+    "titles": "Google",
+    "prop": "extlinks",
+    "format": "json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print(DATA)
