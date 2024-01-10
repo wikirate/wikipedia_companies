32a454f33dffef3124a8e98ce86e1e41165839b9
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2024-01-10 16:09:56 -0700

    get wikirate companies and fetch company links
    ..from Wikipedia sites

--- wikipedia_companies.py
+++ wikipedia_companies.py
@@ -1,31 +1,6 @@
-# Two approaches to find the Official Website link from Wikipedia:
-# 1. Wikipedia API call -> list of external extract_links_with_text
-# This approach needs further processing to extract the actual official website
-# Returns a list of external links
-
 import requests
-
-S = requests.Session()
-
-URL = "https://en.wikipedia.org/w/api.php"
-
-PARAMS = {
-    "action": "query",
-    "titles": "Google",
-    "prop": "extlinks",
-    "format": "json"
-}
-
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
-
-print(DATA)
-
-# 2. This approach scrapes the Wikipedia company page and returns the Official website.
-# The drawback is that the Company name should be a pretty good match. 
-# For example 'Apple' refers to the yummy fruit.
-
 from bs4 import BeautifulSoup
+from decouple import config
 
 def fetch_wikipedia_content(page_title):
     URL = f"https://en.wikipedia.org/wiki/{page_title}"
@@ -39,18 +14,35 @@ def extract_links_with_text(page_content, link_text):
     links = soup.find_all('a', text=link_text)
     return [link['href'] for link in links]
 
-company_name = "Apple Inc"
+def extract_official_website_links(wikipedia_urls, link_text_to_find):
+    official_links_per_company = {}
+
+    for company_url in wikipedia_urls:
+        page_content = fetch_wikipedia_content(company_url)
+        
+        if page_content:
+            official_links = extract_links_with_text(page_content, link_text_to_find)
+            official_links_per_company[company_url] = official_links
+        else:
+            official_links_per_company[company_url] = []
+
+    return official_links_per_company
+
+from wikirate4py import API
+
+api = API(config('API_KEY'))
+companies = api.get_companies()
+
+wikipedia_urls = [company.wikipedia_url for company in companies if company.wikipedia_url is not None]
 link_text_to_find = "Official website"
 
-page_content = fetch_wikipedia_content(company_name)
-if page_content:
-    official_links = extract_links_with_text(page_content, link_text_to_find)
+official_links = extract_official_website_links(wikipedia_urls, link_text_to_find)
 
-    if official_links:
+for company_url, links in official_links.items():
+    print(f"Company: {company_url}")
+    if links:
         print("Official Website Links:")
-        for official_link in official_links:
+        for official_link in links:
             print(official_link)
     else:
         print(f"No official website links found with the link text '{link_text_to_find}'.")
-else:
-    print(f"Failed to fetch Wikipedia content for '{company_name}'.")
