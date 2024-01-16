224100f77a52f17aca6351da7e26ec682ca34ef5
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2024-01-16 13:45:35 -0700

    add insert method and update fetch wikirate companies method

--- wikipedia_companies.py
+++ wikipedia_companies.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from decouple import config
 from wikirate4py import API
 
-class WikipediaLinkExtractor:
+
+class WikipediaCompanyLinks:
     def __init__(self, api_key):
         self.api = API(api_key)
 
     def fetch_wikipedia_content(self, page_title):
-        URL = f"https://en.wikipedia.org/wiki/{page_title}"
+        URL = f'https://en.wikipedia.org/wiki/{page_title}'
         response = requests.get(URL)
         if response.status_code == 200:
             return response.text
@@ -22,37 +23,39 @@ class WikipediaLinkExtractor:
     def extract_official_website_links(self, wikipedia_urls, link_text_to_find):
         official_links_per_company = {}
 
-        for company_url in wikipedia_urls:
-            page_content = self.fetch_wikipedia_content(company_url)
-            
-            if page_content:
-                official_links = self.extract_links_with_text(page_content, link_text_to_find)
-                official_links_per_company[company_url] = official_links
-            else:
-                official_links_per_company[company_url] = []
+        for company_dict in wikipedia_urls:
+            for identifier, page_title in company_dict.items():
+                page_content = self.fetch_wikipedia_content(page_title)
+
+                if page_content:
+                    official_links = self.extract_links_with_text(page_content, link_text_to_find)
+                    official_links_per_company[identifier] = official_links
+                else:
+                    official_links_per_company[identifier] = []
 
         return official_links_per_company
 
     def get_official_website_links(self, link_text_to_find):
         companies = self.api.get_companies()
-        wikipedia_urls = [company.wikipedia_url for company in companies if company.wikipedia_url is not None]
+        wikipedia_urls = [{company.id: company.wikipedia_url} for company in companies if company.wikipedia_url is not None]
 
         official_links = self.extract_official_website_links(wikipedia_urls, link_text_to_find)
 
         return official_links
 
+    def insert_official_company_pages(self, identifier, official_company_link):
+        self.api.update_company(identifier, official_company_link=official_company_link)
+
 
 api_key = config('API_KEY')
-link_text_to_find = "Official website"
+link_text_to_find = 'Official website'
 
-link_extractor = WikipediaLinkExtractor(api_key)
-official_links = link_extractor.get_official_website_links(link_text_to_find)
+company_links = WikipediaCompanyLinks(api_key)
+official_links = company_links.get_official_website_links(link_text_to_find)
 
-for company_url, links in official_links.items():
-    print(f"Company: {company_url}")
+for company_identifier, links in official_links.items():
     if links:
-        print("Official Website Links:")
         for official_link in links:
-            print(official_link)
+            company_links.insert_official_company_pages(company_identifier, official_link)
     else:
-        print(f"No official website links found with the link text '{link_text_to_find}'.")
+        print(f"No official website links found for company {company_identifier} with the link text '{link_text_to_find}'.")
